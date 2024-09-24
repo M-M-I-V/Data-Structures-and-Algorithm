7c4249503e44feff030534c704d3cf4f791d85dd
Neil Jay Lacandazo <138986020+M-M-I-V@users.noreply.github.com>
2024-09-24 16:00:48 +0800

    Update CreateConnection.java

--- CreateConnection.java
+++ CreateConnection.java
@@ -5,20 +5,24 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class CreateConnection {
+	
 	public static void main(String[] args) {
+		
 		String url = "jdbc:mysql://localhost:3306/mcst";
-        String username = "root"; 
-        String password = ""; 
+        	String username = "root"; 
+        	String password = ""; 
 
-        try {
-            Connection connection = DriverManager.getConnection(url, username, password);
+        	try {
+          		Connection connection = DriverManager.getConnection(url, username, password);
 
-            System.out.println("Connected successfully");
+            		System.out.println("Connected successfully");
             
-            connection.close();
+            		connection.close();
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        	} catch (SQLException e) {
+           		 e.printStackTrace();
+        	}
+		
 	}
+	
 }
